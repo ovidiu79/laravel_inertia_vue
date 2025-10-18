619d8cc42feb9b0ae162b6fa85798465c439da61
{-Ovidiu Sandu-} <{-ovy79ro@yahoo.com-}>
2025-10-19 00:10:11 +0300

    wip

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -7,7 +7,12 @@
         email: null,
         password: null,
         password_confirmation: null,
+        avatar: null,
     })
+
+    const change = (e) => {
+        form.avatar = e.target.files[0]
+    }
 
     const submit = () => {
         form.post("/register", {
@@ -24,6 +29,12 @@
 
     <div class="w-2/4 mx-auto">
         <form>
+            <div>
+                <label for="avatar">Avatar</label>
+                <input @input="change" type="file" id="avatar">
+                <p>{{ form.errors.avatar }}</p>
+            </div>
+
             <TextInput name="name" v-model="form.name" :message="form.errors.name" />
             <TextInput name="email" type="email" v-model="form.email" :message="form.errors.email" />
             <TextInput name="password" type="password" v-model="form.password" :message="form.errors.password" />
